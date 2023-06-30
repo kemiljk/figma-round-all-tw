c036299809fdd6a86fe79d9c1030d59a748a50e0
Karl Koch <karl@kejk.tech>
2023-06-30 16:47:25 +0100

    init

--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,204 @@
-figma.showUI(__html__, { themeColors: true, height: 300 });
+figma.showUI(__html__, { themeColors: true, width: 300, height: 104 });
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === "create-rectangles") {
-    const nodes = [];
+const layers = figma.currentPage.findAll();
+let propertiesToRound = ['paddingLeft', 'paddingRight', 'paddingTop', 'paddingBottom', 'itemSpacing'];
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'select-and-run') {
+    await roundPixels().then(() => figma.notify('All cleaned up!'));
   }
-
-  figma.closePlugin();
+  if (msg.type === 'run') {
+    await selectAndRoundType().then(() => figma.notify('All cleaned up!'));
+    if (msg.checkboxOn === true) {
+      figma.closePlugin();
+    }
+  }
 };
+
+async function roundPixels(): Promise<string> {
+  const traverseChildren = async (node: any) => {
+    node.x = Math.round(node.x);
+    node.y = Math.round(node.y);
+    node.resize(Math.round(node.width), Math.round(node.height));
+    node.rotation = Math.round(node.rotation);
+    node.strokeWeight = Math.round(node.strokeWeight);
+
+    switch (node.type) {
+      case 'TEXT':
+        await figma.loadFontAsync(node.fontName as FontName);
+        const { textAutoResize } = node;
+        node.textAutoResize = textAutoResize === 'NONE' ? 'NONE' : textAutoResize === 'HEIGHT' ? 'HEIGHT' : 'WIDTH_AND_HEIGHT';
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.fontSize = Math.round(node.fontSize);
+        const LH = node.getRangeLineHeight(0, node.characters.length);
+
+        if (LH.unit !== 'AUTO' && LH.unit !== 'PERCENT') {
+          node.setRangeLineHeight(0, node.characters.length, {
+            value: Math.round(LH.value),
+            unit: 'PIXELS',
+          });
+        }
+
+        if (LH.unit === 'PERCENT') {
+          node.setRangeLineHeight(0, node.characters.length, {
+            value: Math.round(LH.value),
+            unit: 'PERCENT',
+          });
+        }
+
+        const LS = node.getRangeLetterSpacing(0, node.characters.length);
+
+        if (LS.unit === 'PIXELS') {
+          node.setRangeLetterSpacing(0, node.characters.length, {
+            value: Math.round(LS.value),
+            unit: 'PIXELS',
+          });
+        }
+
+        if (LS.unit === 'PERCENT') {
+          node.setRangeLetterSpacing(0, node.characters.length, {
+            value: Math.round(LS.value),
+            unit: 'PERCENT',
+          });
+        }
+
+        node.paragraphSpacing = Math.round(node.paragraphSpacing);
+        break;
+
+      case 'RECTANGLE':
+      case 'ELLIPSE':
+      case 'POLYGON':
+      case 'STAR':
+      case 'VECTOR':
+      case 'LINE':
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.cornerRadius = Math.round(node.cornerRadius);
+        node.rotation = Math.round(node.rotation);
+        break;
+
+      case 'FRAME':
+      case 'GROUP':
+      case 'COMPONENT':
+      case 'INSTANCE':
+        node.x = Math.round(node.x);
+        node.y = Math.round(node.y);
+        node.resize(Math.round(node.width), Math.round(node.height));
+        node.rotation = Math.round(node.rotation);
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.cornerRadius = Math.round(node.cornerRadius);
+
+        for (const property of propertiesToRound) {
+          node[property] = Math.round(node[property]);
+        }
+
+        for (const child of node.children) {
+          await traverseChildren(child);
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  figma.currentPage.findAll().forEach(async (node) => {
+    await traverseChildren(node);
+  });
+
+  return 'Done!';
+}
+
+async function selectAndRoundType(): Promise<string> {
+  const traverseChildren = async (node: any) => {
+    node.x = Math.round(node.x);
+    node.y = Math.round(node.y);
+    node.resize(Math.round(node.width), Math.round(node.height));
+    node.rotation = Math.round(node.rotation);
+    node.strokeWeight = Math.round(node.strokeWeight);
+
+    switch (node.type) {
+      case 'TEXT':
+        await figma.loadFontAsync(node.fontName as FontName);
+        const { textAutoResize } = node;
+        node.textAutoResize = textAutoResize === 'NONE' ? 'NONE' : textAutoResize === 'HEIGHT' ? 'HEIGHT' : 'WIDTH_AND_HEIGHT';
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.fontSize = Math.round(node.fontSize);
+        const LH = node.getRangeLineHeight(0, node.characters.length);
+
+        if (LH.unit !== 'AUTO' && LH.unit !== 'PERCENT') {
+          node.setRangeLineHeight(0, node.characters.length, {
+            value: Math.round(LH.value),
+            unit: 'PIXELS',
+          });
+        }
+
+        if (LH.unit === 'PERCENT') {
+          node.setRangeLineHeight(0, node.characters.length, {
+            value: Math.round(LH.value),
+            unit: 'PERCENT',
+          });
+        }
+
+        const LS = node.getRangeLetterSpacing(0, node.characters.length);
+
+        if (LS.unit === 'PIXELS') {
+          node.setRangeLetterSpacing(0, node.characters.length, {
+            value: Math.round(LS.value),
+            unit: 'PIXELS',
+          });
+        }
+
+        if (LS.unit === 'PERCENT') {
+          node.setRangeLetterSpacing(0, node.characters.length, {
+            value: Math.round(LS.value),
+            unit: 'PERCENT',
+          });
+        }
+
+        node.paragraphSpacing = Math.round(node.paragraphSpacing);
+        break;
+
+      case 'RECTANGLE':
+      case 'ELLIPSE':
+      case 'POLYGON':
+      case 'STAR':
+      case 'VECTOR':
+      case 'LINE':
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.cornerRadius = Math.round(node.cornerRadius);
+        node.rotation = Math.round(node.rotation);
+        break;
+
+      case 'FRAME':
+      case 'GROUP':
+      case 'COMPONENT':
+      case 'INSTANCE':
+        node.x = Math.round(node.x);
+        node.y = Math.round(node.y);
+        node.resize(Math.round(node.width), Math.round(node.height));
+        node.rotation = Math.round(node.rotation);
+        node.strokeWeight = Math.round(node.strokeWeight);
+        node.cornerRadius = Math.round(node.cornerRadius);
+
+        for (const property of propertiesToRound) {
+          node[property] = Math.round(node[property]);
+        }
+
+        for (const child of node.children) {
+          await traverseChildren(child);
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  figma.currentPage.selection.forEach(async (node) => {
+    await traverseChildren(node);
+  });
+
+  return 'Done!';
+}
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,42 +1,25 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "./ui.css";
-
-declare function require(path: string): any;
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Checkbox from './components/Checkbox';
+import './ui.css';
 
 function App() {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-
-  const onCreate = () => {
-    const count = Number(inputRef.current?.value || 0);
-    parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", count } },
-      "*"
-    );
-  };
-
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-  };
+  const [checkboxOn, setCheckboxOn] = React.useState(false);
 
   return (
-    <main className="bg-white h-[100vh] flex flex-col justify-center items-center w-full">
-      <header className="flex flex-col justify-center items-center mb-4">
-        <img src={require("./logo.svg")} />
-        <h2 className="text-2xl">Rectangle Creator</h2>
-      </header>
-      <section className="flex flex-row justify-center items-center space-x-2  mb-4">
-        <label htmlFor="input">Count</label>
-        <input className="border border-neutral-100 rounded-md p-2" id="input" type="number" min="0" ref={inputRef} />
-      </section>
-      <footer className="flex flex-row justify-center items-center space-x-2">
-        <button className="bg-blue-500 text-white rounded-md py-2 px-3" onClick={onCreate}>
-          Create
+    <main className='w-full p-2'>
+      <Checkbox checkboxOn={checkboxOn} setCheckboxOn={setCheckboxOn} />
+      <div className='flex w-full pt-1 mx-auto'>
+        <button id='run' onClick={() => parent.postMessage({ pluginMessage: { type: 'run', checkboxOn } }, '*')} className='secondary'>
+          Your selection
         </button>
-        <button className="border border-neutral-300 rounded-md py-2 px-3" onClick={onCancel}>Cancel</button>
-      </footer>
+        <button id='select-and-run' onClick={() => parent.postMessage({ pluginMessage: { type: 'select-and-run', checkboxOn } }, '*')} className='primary'>
+          Entire document
+        </button>
+      </div>
     </main>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("react-page"));
+const root = createRoot(document.getElementById('react-page')!);
+root.render(<App />);
